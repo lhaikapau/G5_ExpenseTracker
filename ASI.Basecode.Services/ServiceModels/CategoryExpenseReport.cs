7dea8ea3acf89fbda0e2a496a4f5a7bb9c68242c
Jeffandrewo <andrewdandan80@gmail.com>
2024-11-03 11:43:00 +0800

    Added Total Expense on HomeController and some Ui changes

---
+++ ASI.Basecode.Services/ServiceModels/CategoryExpenseReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.ServiceModels
+{
+    public class CategoryExpenseReport
+    {
+        public int? CategoryId { get; set; }
+        public double TotalAmount { get; set; }
+    }
+}
