3d900936f5e754c6af142fa58d94d56ef15ac077
21-2057-605 Aubrey Pagdalian <aubrey.pagdalian@cit.edu>
2024-11-08 20:41:45 +0800

    login UI & register UI

--- ASI.Basecode.Resources/Views/Screen.Designer.cs
+++ ASI.Basecode.Resources/Views/Screen.Designer.cs
@@ -61,7 +61,7 @@ namespace ASI.Basecode.Resources.Views {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ©2023 Alliance Software Inc. All rights reserved..
+        ///   Looks up a localized string similar to ©2024 Packet Pal Tracker. All rights reserved..
         /// </summary>
         public static string Copyright {
             get {
