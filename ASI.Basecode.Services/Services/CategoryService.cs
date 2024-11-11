25d49930bb308111bb6907ed925fcfe6cde64c6a
jepenavaja <johnpaul.navaja@cit.edu>
2024-11-11 15:18:44 +0800

    jp - merged na tanan

--- ASI.Basecode.Services/Services/CategoryService.cs
+++ ASI.Basecode.Services/Services/CategoryService.cs
@@ -25,19 +25,8 @@ namespace ASI.Basecode.Services.Services
             _config = configuration;
         }
 
-        public bool IsCategoryNameExists(string name, string userId)
-        {
-            return _categoryRepository.RetrieveAll()
-                .Any(c => c.Name.ToLower() == name.ToLower() && c.CreatedBy == userId);
-        }
-
         public void AddCategory(CategoryViewModel model, string userId)
         {
-            if (IsCategoryNameExists(model.Name, userId))
-            {
-                throw new InvalidOperationException("A category with this name already exists.");
-            }
-
             var newCategory = new Category();
             newCategory.Name = model.Name;
             newCategory.Description = model.Description;
@@ -53,7 +42,6 @@ namespace ASI.Basecode.Services.Services
             var data = _categoryRepository
                 .RetrieveAll()
                 .Where(c => c.CreatedBy == UserId) // Filter by userId
-                .OrderByDescending(c => c.DateCreated)
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .Select(s => new CategoryViewModel
@@ -121,15 +109,6 @@ namespace ASI.Basecode.Services.Services
             var category = _categoryRepository.RetrieveAll().Where(x => x.CategoryId.Equals(model.CategoryId)).FirstOrDefault();
             if (category != null)
             {
-                // Check if the new name exists for any other category
-                if (_categoryRepository.RetrieveAll()
-                    .Any(c => c.Name.ToLower() == model.Name.ToLower()
-                        && c.CreatedBy == userId
-                        && c.CategoryId != model.CategoryId))
-                {
-                    throw new InvalidOperationException("A category with this name already exists.");
-                }
-
                 category.Name = model.Name;
                 category.Description = model.Description;
                 category.DateUpdated = DateTime.Now;
@@ -147,7 +126,5 @@ namespace ASI.Basecode.Services.Services
                 _categoryRepository.DeleteCategory(category);
             }
         }
-
-
     }
 }
